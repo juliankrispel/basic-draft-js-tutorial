16d29d7a83d5373153a99cf89139d3db44d4076a
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2017-06-16 13:57:33 +0100

    added rich text key commands

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 class App extends Component {
   constructor() {
@@ -13,10 +13,22 @@ class App extends Component {
     this.setState({ editorState });
   };
 
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
   render() {
     return (
       <Editor
         editorState={this.state.editorState}
+        handleKeyCommand={this.handleKeyCommand}
         onChange={this.onChange}
       />
     );
